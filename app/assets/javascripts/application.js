b661655d26488e372cd63a6228d05143c1a9daa7
César Duarte <cesardh777@gmail.com>
2016-08-23 10:06:20 -0500

    boton arreglado

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,21 +17,20 @@
 
 
 $(document).ready(function(){
-  var alto = $(window).height();
   $("body").append('<div id="pre-load-web">cargando</div>');
   $("#pre-load-web").css({
-    height: alto+"px",
+    height: "100%",
     background: "white",
     width: '100%',
     position: "absolute",
     zIndex: "200"});
 
-  var templo = new Parallax( $('.templo'), -0.02);
-  var logo = new Parallax( $('.logo'), -0.07);
-  var monje = new Parallax( $('.monje'), -0.09);
+  templo = new Parallax( $('.templo'), -0.02);
+  logo = new Parallax( $('.logo'), -0.07);
+  monje = new Parallax( $('.monje'), -0.09);
   var globoVideo = new Parallax( $('#g_video'), -0.12);
   var bambu1 = new Parallax( $('.bambu1'), -0.15);
-  var bambu2 = new Parallax( $('.bambu2'), -0.15);
+  bambu2 = new Parallax( $('.bambu2'), -0.15);
   globoPlay = new Parallax( $('#g_play'), -0.12);
 
   var globo = [];
@@ -46,7 +45,6 @@ $(document).ready(function(){
 
 
   for (var i = 0; i < 5; i++) {
-
     $('.globo' + i).css({
       width: globoPos[i][0] + '%',
       right: globoPos[i][1] + '%',
@@ -55,35 +53,47 @@ $(document).ready(function(){
 
     var random = Math.random() * 0.15 + 0.05;
     globo[i] = new Parallax( $('.globo' + i)  , -random);
-
   };
 
-
-
   $('#wrapper').mousemove(function(event){
-
     var wcenter = $(window).width()/2;
     var hcenter = $(window).height()/2;
     mx = event.pageX - wcenter;
     my = event.pageY - hcenter;
 
-    monje.mover();
-    logo.mover();
-    templo.mover();
-    bambu1.mover();
-    bambu2.mover();
-    globoVideo.mover();
-    globoPlay.mover();
-
+    monje.animar();
+    logo.animar();
+    templo.animar();
+    bambu1.animar();
+    bambu2.animar();
+    globoVideo.animar();
+    globoPlay.animar();
 
     for (var i = 0; i < globo.length; i++) {
-      //globo[i].vy = -globo[i].vx * 2;
-      globo[i].mover()
+      globo[i].animar()
     }
+  });
 
-  })
+  setInterval(draw,40);
+  function draw(){
+    monje.mostrar();
+    logo.mostrar();
+    templo.mostrar();
+    bambu1.mostrar();
+    bambu2.mostrar();
+    globoVideo.mostrar();
+    globoPlay.mostrar();
 
-  $('#g_play').on('click', scroll);
+    for (var i = 0; i < globo.length; i++) {
+      globo[i].mostrar()
+    }
+  }
+
+  var bol = true;
+  $('#g_play').on('click', function(){
+    scrolling(bol);
+    bol = !bol;
+  });
 
 });
 
@@ -101,48 +111,72 @@ function Parallax(elem, vel){
   this.vx = vel;
   this.vy = vel/2
   this.elem = elem;
-  this.trX = 0;
-  this.elemX = this.elem.position().left + this.trX;
+  this.elemX = this.elem.position().left;
   this.elemY = this.elem.position().top;
+  this.posX;
+  this.posY;
+  this.trX = 0;
+  this.trY = 0;
 
-  this.mover = function(){
-    var posX = this.elemX + mx * this.vx;
-    var posY = this.elemY + my * this.vy;
-
-    this.elem.css('left', posX + 'px');
-    this.elem.css('top', posY + 'px');
+  this.animar = function(){
+    this.posX = this.elemX + mx * this.vx;
+    this.posY = this.elemY + my * this.vy;
   }
 
-  this.trasladar = function(left, top, vel) {
-    this.left = this.left;
-    this.elem.animate({ left: '+='+left+'px', top: '+='+top+'px' }, {
-      duration: vel,
-      step: function(now, fx){
-
-        if (fx.prop == 'left'){
-          this.left = fx.now - fx.start;
-          console.log('1-' + this.left)
-        }
-        console.log('2-' + this.left);
-      }
-    });
-    this.trX = this.left;
+  this.mostrar = function(){
+    this.elem.css('left', this.posX + this.trX + 'px');
+    this.elem.css('top', this.posY + this.trY + 'px');
   }
 
 }
 
-var globoPlay;
+function desplazar(objeto, left, top, time) {
 
-function scroll(){
+  objeto.elem.animate({
+    countL: left,
+    countT: top
+  },
+  {
+    duration: time,
+    step: function(now, fx){
+      if (fx.prop == 'countL') {
+        objeto.trX = fx.now;
+      } else if(fx.prop == 'countT'){
+        objeto.trY = fx.now;
+      }
+    }
+  });
+}
+
+var globoPlay,
+    logo,
+    monje,
+    templo,
+    bambu2;
+
+function scrolling(boolean){
   var ww = $(window).width();
-  if (globoPlay.elemX < 1000) {
-    //globoPlay.elemX += 400;
-    globoPlay.trasladar(400, 0, 3000);
-    $('#wrapper').animate({scrollLeft: ww + 'px'}, 2000);
-  } else {
-    globoPlay.elemX -= 400;
-    $('#wrapper').animate({scrollLeft: '0'}, 2000);
-  }
+  var x = 400;
+  var t = 2500;
+
+  if (boolean == true) {
+    desplazar(globoPlay, x, 0, t);
+    desplazar(logo, ww/1.2, -50, 1500);
+    desplazar(monje, -1000, 0 , 1500);
+    desplazar(templo, 0, -1000 , 1500)
+    desplazar(bambu2, 100, 0, 3000);
+    $('.logo').animate({width: '30%'},1000);
+    $('#wrapper').animate({scrollLeft: ww + 'px'}, t);
+
+  }else if (boolean == false) {
+    desplazar(globoPlay, 0, 0, t);
+    $('.logo').animate({width: '45%'},1000);
+    desplazar(logo, 0, 0, 2000);
+    $('#wrapper').animate({scrollLeft: '0'}, t);
+    desplazar(monje, 0, 0 , 3200);
+    desplazar(templo, 0, 0 , 3000);
+    desplazar(bambu2, 0, 0, 3000);
+  };
 }
 
 
